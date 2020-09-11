06810df925b27bdd8bc50b6f1621e4159be6af67
wzq016 <wzq016@gmail.com>
2020-09-11 21:15:00 +0800

    fix history

--- NewsApp/app/src/main/java/com/example/newsapp/ui/history/HistoryFragment.java
+++ NewsApp/app/src/main/java/com/example/newsapp/ui/history/HistoryFragment.java
@@ -1,18 +1,23 @@
 package com.example.newsapp.ui.history;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.newsapp.Activity.ShowNewsActivity;
 import com.example.newsapp.Adapter.NewsAdapter;
 import com.example.newsapp.Data.CovidNews;
+import com.example.newsapp.Data.CovidNewsWithText;
 import com.example.newsapp.Data.HistoryManager;
 import com.example.newsapp.Data.NewsManager;
 import com.example.newsapp.R;
+import com.example.newsapp.Thread.GetContentThread;
 
 import java.util.ArrayList;
 
@@ -22,11 +27,13 @@ public class HistoryFragment extends Fragment {
     private ArrayList<CovidNews> newslist;
     private HistoryManager historyManager;
     private NewsAdapter newsAdapter;
+    private NewsManager newsManager;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         view = inflater.inflate(R.layout.fragment_history,container,false);
         listView = view.findViewById(R.id.history_listview);
+        newsManager = new NewsManager();
         init_listview();
 
         return view;
@@ -36,10 +43,29 @@ public class HistoryFragment extends Fragment {
         newslist = new ArrayList<CovidNews>();
         historyManager = new HistoryManager();
         newslist.addAll(historyManager.getNewsHistory());
-        System.out.println("GetHistorylen"+newslist.size());
+
         newsAdapter = new NewsAdapter(getContext(), R.layout.one_news, newslist);
         listView.setAdapter(newsAdapter);
         listView.setVisibility(View.VISIBLE);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getContext(), ShowNewsActivity.class);
+                intent.putExtra("title", newslist.get(i - listView.getHeaderViewsCount()).getTitle());
+                intent.putExtra("date", newslist.get(i - listView.getHeaderViewsCount()).getDate());
+
+                intent.putExtra("source", newslist.get(i - listView.getHeaderViewsCount()).getSource());
+
+                CovidNewsWithText temp = newsManager.getOfflineNews(newslist.get(i - listView.getHeaderViewsCount()));
+
+
+                intent.putExtra("body", temp.getContent());
+
+                startActivity(intent);
+            }
+
+        });
     }
 
 
